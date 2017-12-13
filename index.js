59426f10e9aa3551f4a5f231b410152195c364fb
Yavor Punchev <yavor.punchev@gmail.com>
2017-12-13 15:57:33 +0000

    Export colors

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-var postcss = require('postcss');
+const postcss = require('postcss');
+const readFile = require('fs-readfile-promise');
+const writeFile = require('fs-writefile-promise/lib/node7');
 
-module.exports = postcss.plugin('postcss-design-system', function (opts) {
-    opts = opts || {};
+// On rejected promise
+const onRejected = err => console.log(err);
 
-    // Work with options here
+// On fulfilled promise
+const onFulfilled = filename => console.log(filename);
 
-    return function (root, result) {
+// Convert the colors object to variables
+const parseColors = colors => {
+    let output = ':root{\n';
 
-        // Transform CSS AST here
+    Object.entries(colors).forEach(([key, value]) => {
+        output += '  --' + key + ': "' + value + '";\n';
+    });
 
+    output += '}';
+    return output;
+};
+
+// Read JSON file, convert to variables and write to a new file
+const readAndWrite = (buffer, output) => {
+    const data = JSON.parse(buffer.toString());
+    const colors = parseColors(data.colors);
+
+    writeFile(output, colors)
+        .then(onFulfilled)
+        .catch(onRejected);
+};
+
+module.exports = postcss.plugin('postcss-design-system', function (options) {
+    options = options || {
+        inputFile: './theme.json',
+        outputFile: './theme.css'
     };
+
+    // Input and output destination come from options
+    readFile(options.inputFile)
+        .then(buffer => readAndWrite(buffer, options.outputFile))
+        .catch(onRejected);
 });
--- index.test.js
+++ index.test.js
@@ -3,7 +3,8 @@ var postcss = require('postcss');
 var plugin = require('./');
 
 function run(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
+    return postcss([plugin(opts)])
+        .process(input)
         .then(result => {
             expect(result.css).toEqual(output);
             expect(result.warnings().length).toBe(0);
